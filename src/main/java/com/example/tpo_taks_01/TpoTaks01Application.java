04f6e113dfcd7f2f5a536949d359b7eac93818c5
BilyiPJATK <s31718@pjwstk.edu.pl>
2025-03-03 14:47:50 +0100

    TPO task 1 done

---
+++ src/main/java/com/example/tpo_taks_01/TpoTaks01Application.java
@@ -0,0 +1,14 @@
+package com.example.tpo_taks_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoTaks01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpoTaks01Application.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/tpo_taks_01/TpoTaks01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpo_taks_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoTaks01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
